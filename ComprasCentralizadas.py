e25a394de5dc278be5950cc3970f410e03519ffd
Joao Pereira <jluizpjr@gmail.com>
2023-07-24 12:34:10 -0300

    OK

--- ComprasCentralizadas.py
+++ ComprasCentralizadas.py
@@ -14,15 +14,9 @@ import pandas as pd
 import os
 import sys
 
-print("CNAE="+sys.argv[1])
-
-if not sys.argv[1]:
-    cnae='6203100'
-else:
-    cnae=str(sys.argv[1])
 
 base_url = 'https://compras.dados.gov.br'
-fornecedor_base_page_url = '/fornecedores/v1/fornecedores.json?id_cnae='+cnae
+fornecedor_base_page_url = '/fornecedores/v1/fornecedores.json?id_cnae='
 contratos_base_page_url = '/comprasContratos/v1/contratos.json?fornecedor_cnpj_cpf_idgener='
 fornecedores = []
 contratos = []
@@ -36,52 +30,53 @@ def AnexaProximasPaginas(links):
         response = requests.get(base_url+fornecedor_next_page_url)
         resp_dict = response.json()
         temp = resp_dict['_embedded']['fornecedores']
-        print("Fornecedores na proxima pagina:"+str(len(temp)))
         fornecedores = fornecedores + temp
         AnexaProximasPaginas(resp_dict['_links'])
     return 
 
 #Gera lista detalhada de contratos por CNPJ
 def ConsultaContratos(fornecedor):
-    #global contratos
     cnpj = fornecedor['cnpj']
     if not os.path.exists("./"+cnae+'/'+cnpj+".csv"):
         cnpj_fmt =  '{}.{}.{}/{}-{}'.format(cnpj[:2], cnpj[2:5], cnpj[5:8], cnpj[8:12], cnpj[12:])  
         response = requests.get(base_url+contratos_base_page_url+cnpj_fmt)
         resp_dict = response.json()
-        #print("Contratos: "+str(len(resp_dict['_embedded']['contratos'])))
         contratos = resp_dict['_embedded']['contratos']
-        #contratos = contratos + temp
         print("Total de Contratos para cnpj "+cnpj_fmt+": "+str(len(contratos)))
-        #df = pd.read_json(contratos, orient='records')
         df = pd.json_normalize(contratos)
-
         if len(contratos) > 0:
             if not os.path.exists(cnae):
                 os.mkdir(cnae)
             df.to_csv(cnae+'/'+cnpj+'.csv', sep="|")
-        #print(df)
     return
 
 
-response = requests.get(base_url+fornecedor_base_page_url)
-resp_dict = response.json()
-fornecedores = resp_dict['_embedded']['fornecedores']
+def BaixaContratos(cnae):
 
-print("Quantidade de fornecedores para o CNAE:" + str(len(fornecedores)))
-exit
+    response = requests.get(base_url+fornecedor_base_page_url+cnae)
+    resp_dict = response.json()
+    fornecedores = resp_dict['_embedded']['fornecedores']
 
-if len(fornecedores) > 499: 
-    AnexaProximasPaginas(resp_dict['_links'])
+    if len(fornecedores) > 499: 
+        AnexaProximasPaginas(resp_dict['_links'])
 
-print("Quantidade de fornecedores para o CNAE:" + str(len(fornecedores)))
+    print("Quantidade de fornecedores para o CNAE:" + str(len(fornecedores)))
 
-for fornecedor in fornecedores:
-    #print(fornecedor['nome'])
-    try:
-        ConsultaContratos(fornecedor)
-    except Exception as e:
-        print("Erro na leitura do JSON")
+    for fornecedor in fornecedores:
+        try:
+            ConsultaContratos(fornecedor)
+        except Exception as e:
+            print("Erro na leitura do JSON")
+    return
+
+
+file_cnae = open('cnae.txt', 'r')
+lines = file_cnae.readlines()
+
+for line in lines:
+    print("Processando CNAE: "+line.strip())
+    BaixaContratos(line.strip())
+    print("Fim CNAE:"+line.strip())
 
 print("Fim de processamento.")
 
